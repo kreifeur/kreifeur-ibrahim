4f7794c156a0f8b63a9f43c49540b8a3d8f3fb97
kreifeur <kreifeur669@gmail.com>
2023-12-22 14:18:57 +0100

    fixing contact error

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className="text-sm main">
         <Navbar />
         {children}
-       {/*  <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
